5e04c39d63962bd3160601164825f0088e13451d
utkarsh032 <utkarshhraj525@gmail.com>
2023-07-02 00:03:50 +0530

    Advantages of Express js-- (Nodemon)

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
 const express = require('express')
+const bodyParser = require('body-Parser')
 
 const app = express()
 
-app.use((req, res, next) => {
-  let body = ''
-  req.on('end', () => {
-    const userName = body.split('=')[1]
-    if (userName) {
-      req.body = { name: userName }
-    }
-    next()
-  })
-  req.on('data', chunk => {
-    body += chunk
-  })
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.post('/user', (req, res, next) => {
+  res.send('<h1>User: ' + req.body.username + '</h1>')
 })
 
-app.use((req, res, next) => {
-  if (req.body) {
-    return res.send('<h1>' + req.body.name + '</h1>')
-  }
-
+app.get('/', (req, res, next) => {
   res.send(
-    '<form method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>'
+    '<form action="/user" method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>'
   )
 })
 
