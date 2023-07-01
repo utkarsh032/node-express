4fbe2b3b82ae57292d2c0bd37b099062ca967bee
utkarsh032 <utkarshhraj525@gmail.com>
2023-07-01 23:39:18 +0530

    Express js--

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
-const http = require('http')
+const express = require('express')
 
-const server = http.createServer((req, res) => {
-  console.log('INCOMING REQUEST')
-  console.log(req.method, req.url)
+const app = express()
 
-  if (req.method === 'POST') {
-    let body = ''
-    req.on('end', () => {
-      const userName = body.split('=')[1]
-      res.end('<h1>' + userName + '</h1>')
-    })
-
-    req.on('data', chunk => {
-      body += chunk
-    })
-  } else {
-    res.setHeader('Content-Type', 'text/html')
-    res.end(
-      '<form method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>'
-    )
-  }
+app.use((req, res, next) => {
+  let body = ''
+  req.on('end', () => {
+    const userName = body.split('=')[1]
+    if (userName) {
+      req.body = { name: userName }
+    }
+    next()
+  })
+  req.on('data', chunk => {
+    body += chunk
+  })
 })
 
-server.listen(5000)
+app.use((req, res, next) => {
+  if (req.body) {
+    return res.send('<h1>' + req.body.name + '</h1>')
+  }
+
+  res.send(
+    '<form method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>'
+  )
+})
+
+app.listen(5000)
